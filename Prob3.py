20adbe19080836cdc6054f1368b4da0afe60dd87
Nathan-Diaz <ngarciadiaz@willamette.edu>
2022-04-18 17:56:27 -0700

    ???

--- Prob3.py
+++ Prob3.py
@@ -4,7 +4,6 @@
 # Estimated time spent (hr):
 #######################################
 
-from re import X
 from pgl import GWindow, GRect, GOval, GLine, GLabel
 
 ### Prepatory Code ###
@@ -18,16 +17,11 @@ x_origin = cx / 2
 
 def draw_flag(): #Code that creates the flag
     gw = GWindow(width, height)
-    draw_stripe() #creates rainbow
-    draw_triangle() #adds singular triangle b/c I couldn't test and thus figure out how to make a triangle based outside of a two for loops
-    make_text() #adds text
-    
-def make_text():
-    title = GLabel("Semi-Progessive Pride Flag", x_origin, height) # I am unsure if this is centered
-    title.set_font("bold 80px 'serif'")
-    gw.add(title)
+    draw_stripe(gw) #creates rainbow
+    draw_triangle(gw) #adds singular triangle b/c I couldn't test and thus figure out how to make a triangle based outside of a two for loops
+    make_text(gw) #adds text
 
-def draw_stripe():
+def draw_stripe(gw):
     for y in range(height): # y is defined first because triangle will be created ontop of the stripes
         for x in range(width):
             stripe = GRect(x, y, 1, 1) # starts the stripes
@@ -35,16 +29,25 @@ def draw_stripe():
             stripe.set_color(stripe_colors[stripe_number]) # defines the stripe color based on 
             gw.add(stripe) # adds stripes by color one at a time
 
-def draw_triangle(): # Draws a 45-45-90 triangle of hypotenuse equal to height with hypotenuse along the x == 0 line.
-    for y in range(height):
-        for x in range(width):
-            blue = GRect(x,y,1,1)
-            dis_x = x
-            dis_y = abs((height // 2) - y)
-            dist_total = dis_x + dis_y
-        
-        if dist_total < height // 2:
-            blue.set_color("cyan")
-            gw.add(blue)
+def draw_triangle():
+        # Draws a 45-45-90 triangle of hypotenuse equal to height with hypotenuse along the x == 0 line.
+    
+        # We check (x,y) to see if the total distance of x from the left and y from the middle is greater than 500 // 2 == 25-
+        # If not, we place a place rectangle at that x,y location.
+        for y in range(height):
+            for x in range(width):
+                r = GRect(x,y,1,1)
+                dist_x = x # distance from left
+                dist_y = abs((height // 2) - y) # distance from middle
+                dist_total = dist_x + dist_y
+                
+                if dist_total < height // 2:
+                    r.set_color("black")
+                    gw.add(r)
+
+def make_text(gw):
+    title = GLabel("Semi-Progessive Pride Flag", x_origin, height) # I am unsure if this is centered
+    title.set_font("bold 20px 'serif'")
+    gw.add(title)
 
 draw_flag()
